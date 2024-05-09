ac3927b8530146bbfe6b42023d22862bab5cb265
Juan Pablo Pineda Madrigal <juanpablopinedamadrigal@MacBook-Air-de-Juan.local>
2024-05-09 00:36:33 -0500

    Actualizacion

--- Database.js
+++ Database.js
@@ -1,3 +1,5 @@
+//Database.js
+//Este es mi archivo principal de backend
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
@@ -8,29 +10,16 @@ const multer = require('multer');
 const fs = require('fs');
 const ExcelJS = require('exceljs');
 const mime = require('mime-types');
-
+const loginRouter = require('./endpoints/usuarios/login');
 const app = express();
-app.use(express.json());
 
-// Configurar body-parser
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Configurar CORS
 app.use(cors());
-
-
-// Configuración de la conexión a la base de datos
-const connection = mysql.createConnection({
-  host: 'database-starbucks.c76ow40y46ib.us-east-2.rds.amazonaws.com',
-  user: 'JuanJS',
-  password: 'QyYDKsD8kavrHw8Pikx5',
-  database: 'Starbucks',
-});
-// ----------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------
-// Configurar middleware para servir archivos estáticos desde la carpeta 'CafeStarbucks'
+app.use('/usuarios', loginRouter);
 app.use(express.static(path.join(__dirname, 'CafeStarbucks')));
+
 
 // Configurar el endpoint para servir archivos CSS y JavaScript dinámicamente
 app.get('/archivos.css/:nombreArchivo', (req, res) => {
@@ -346,30 +335,6 @@ app.post('/registrar', (req, res) => {
 });
 //HASTA ACA
 // Manejar solicitudes POST a la ruta '/login'
-app.post('/login', (req, res) => {
-  // Obtener el email y la contraseña del cuerpo de la solicitud
-  const { email, password } = req.body;
-
-  // Consultar la base de datos para verificar las credenciales del usuario
-  const sql = 'SELECT * FROM usuarios WHERE email = ? AND password = ?';
-  connection.query(sql, [email, password], (error, results) => {
-    if (error) {
-      console.error('Error al verificar las credenciales del usuario:', error);
-      return res.status(500).send('Error interno del servidor');
-    }
-
-    // Comprobar si se encontraron resultados en la consulta
-    if (results.length > 0) {
-      // Si se encontró un usuario con las credenciales proporcionadas, enviar los datos del usuario como respuesta
-      const user = results[0];
-      res.status(200).json({ name: user.name, email: user.email });
-    } else {
-      // Si no se encontró ningún usuario con las credenciales proporcionadas, enviar un mensaje de error
-      res.status(401).send('Credenciales incorrectas');
-    }
-  });
-});
-
 
 // Iniciar el servidor
 const PORT = 3000;
---
+++ endpoints/usuarios/login.js
@@ -0,0 +1,39 @@
+// login.js
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
+
+// Configuración de la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: 'database-starbucks.c76ow40y46ib.us-east-2.rds.amazonaws.com',
+  user: 'JuanJS',
+  password: 'QyYDKsD8kavrHw8Pikx5',
+  database: 'Starbucks',
+});
+
+// Endpoint para el login
+router.post('/login', (req, res) => {
+  // Obtener el email y la contraseña del cuerpo de la solicitud
+  const { email, password } = req.body;
+
+  // Consultar la base de datos para verificar las credenciales del usuario
+  const sql = 'SELECT * FROM usuarios WHERE email = ? AND password = ?';
+  connection.query(sql, [email, password], (error, results) => {
+    if (error) {
+      console.error('Error al verificar las credenciales del usuario:', error);
+      return res.status(500).send('Error interno del servidor');
+    }
+
+    // Comprobar si se encontraron resultados en la consulta
+    if (results.length > 0) {
+      // Si se encontró un usuario con las credenciales proporcionadas, enviar los datos del usuario como respuesta
+      const user = results[0];
+      res.status(200).json({ name: user.name, email: user.email });
+    } else {
+      // Si no se encontró ningún usuario con las credenciales proporcionadas, enviar un mensaje de error
+      res.status(401).send('Credenciales incorrectas');
+    }
+  });
+});
+
+module.exports = router;
