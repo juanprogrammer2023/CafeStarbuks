56893c1589254c952735584123d8eb16af3ebd5d
Juan Pablo Pineda Madrigal <juanpablopinedamadrigal@MacBook-Air-de-Juan.local>
2024-05-09 00:59:43 -0500

    Prueba

--- Database.js
+++ Database.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/usuarios', loginRouter);
+app.use('/', loginRouter);
 app.use(express.static(path.join(__dirname, 'CafeStarbucks')));
 
 
