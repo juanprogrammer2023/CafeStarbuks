26f67de44faee48a9a97a5e60b2d11cc84f24951
Juan Pablo Pineda Madrigal <juanpablopinedamadrigal@MacBook-Air-de-Juan.local>
2024-05-10 03:53:26 -0500

    devolucion

--- Database.js
+++ Database.js
@@ -10,7 +10,6 @@ const multer = require('multer');
 const fs = require('fs');
 const ExcelJS = require('exceljs');
 const mime = require('mime-types');
-const loginRouter = require('./endpoints/usuarios/login');
 const app = express();
 
 
@@ -26,8 +25,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'CafeStarbucks')));
-app.use('/', loginRouter); 
 
+
+app.post('/login', (req, res) => {
+  
+  // Obtener el email y la contraseña del cuerpo de la solicitud
+  const { email, password } = req.body;
+  
+  // Consultar la base de datos para verificar las credenciales del usuario
+  const sql = 'SELECT * FROM usuarios WHERE email = ? AND password = ?';
+  connection.query(sql, [email, password], (error, results) => {
+    if (error) {
+    
+      console.error('Error al verificar las credenciales del usuario:', error);
+      return res.status(500).send('Error interno del servidor');
+    }
+
+    // Comprobar si se encontraron resultados en la consulta
+    if (results.length > 0) {
+      // Si se encontró un usuario con las credenciales proporcionadas, enviar los datos del usuario como respuesta
+      const user = results[0];
+      res.status(200).json({ name: user.name, email: user.email });
+    } else {
+      // Si no se encontró ningún usuario con las credenciales proporcionadas, enviar un mensaje de error
+      res.status(401).send('Credenciales incorrectas');
+    }
+  });
+});
 
 const ExpReg = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?\/~`-]).+$/;
 //ENDPOINT PARA REGISTRARSE ACTUALIZACION//
