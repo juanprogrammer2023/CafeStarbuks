69cabedb05cd51cc273e4fbe565503c53d6c7218
Juan Pablo Pineda Madrigal <juanpablopinedamadrigal@MacBook-Air-de-Juan.local>
2024-05-16 21:33:15 -0500

    actual

--- endpoints/usuarios/registro.js
+++ endpoints/usuarios/registro.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const ExpReg = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?\/~`-]).+$/;
 
 router.post('', (req, res) => {
-  alert(req.body);  
+  console.log(req.body);  
   const { name, email, password, confirmPassword } = req.body;
 
   if (password !== confirmPassword) {
